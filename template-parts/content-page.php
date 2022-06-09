0d122f45f0e2c9fee2dfd4837ede40f6c299c0c3
Lucas García <lucas@codeccoop.org>
2022-06-09 16:44:54 +0200

    Page parallax updates

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -21,20 +21,4 @@ $page_ID = get_the_ID();
         the_content();
         ?>
     </div><!-- .entry-content -->
-    <?php
-    $parallax = get_field('page-parallax', $page_ID);
-    if ($parallax['show']) : ?>
-        <div class="page-parallax">
-            <div class="page-parallax__background">
-                <?php
-                $image_ID = $parallax['image'];
-                $image_src = wp_get_attachment_image_src($image_ID, 'full', false)[0];
-                ?>
-                <img src="<?= $image_src ?>" alt="Imatge de fons del parallax" aria-hidden="true">
-            </div>
-            <div class="page-parallax__overlay">
-                <?= $parallax['text']; ?>
-            </div>
-        </div>
-    <?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
