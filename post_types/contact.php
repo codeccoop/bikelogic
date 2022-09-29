7707ccecca624d2ae19fec7d8c712d80fade9957
Lucas García <lucas@codeccoop.org>
2022-09-29 12:24:26 +0200

    Update file modes

--- post_types/contact.php
+++ post_types/contact.php
@@ -1,22 +1,23 @@
 <?php
 //adding our own portfolio item
-function bl_post_contact_item() {
+function bl_post_contact_item()
+{
     $labels = array(
-        'name'               => _x( 'formularis contacte', 'post type general name' ),
-        'singular_name'      => _x( 'formulari contacte', 'post type singular name' ),
+        'name'               => _x('formularis contacte', 'post type general name'),
+        'singular_name'      => _x('formulari contacte', 'post type singular name'),
         'menu_name'          => 'Formularis Contacte'
-      );
+    );
     $args = array(
         'labels'        => $labels,
         'description'   => 'Aquest post serveix per crear els formularis de contacte de les pàgines de navegació interna',
         'public'        => true,
         'menu_position' => 6,
-        'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+        'supports'      => array('title', 'editor', 'thumbnail', 'excerpt'),
         'show_in_rest'  => true,
         'has_archive'   => false,
-      );
-    register_post_type( 'formulari-contacte', $args );
+    );
+    register_post_type('formulari-contacte', $args);
 }
 
-add_action( 'init', 'bl_post_contact_item' );
-?>
+add_action('init', 'bl_post_contact_item');
+
